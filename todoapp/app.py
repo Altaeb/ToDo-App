f8bfd406de904ad87e264f22a561d8c2ec4f9873
aftiyah <abd2010ly@gmail.com>
2020-05-02 14:02:17 +0200

    Implementing Reads

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description' : 'Todo 1'
-    },{
-        'description' : 'Todo 2'
-    },{
-        'description' : 'Todo 3'
-    },{
-        'description' : 'Todo 4'
-    }])
+  return render_template('index.html', data=Todo.query.all())
